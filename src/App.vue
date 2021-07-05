96c9af7c3706c4a913f9c4c23552c620f17fcd5a
Mosabberul Islam <mosabberulislam55@gmail.com>
2021-07-05 12:40:17 +0600

    all done

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <h1>Todos</h1>
+    <h3>Completed: {{completedTodos}}</h3>
+    <h3>Pending: {{pendingTodos}}</h3>
+   <todos-list />
+    <todo-form />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TodosList from './components/TodosList.vue'
+import TodoForm from './components/TodoForm.vue'
+import {mapGetters} from 'vuex'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TodosList,
+    TodoForm
+  },
+  computed:{
+    ...mapGetters({
+      completedTodos: "completedTodos",
+      pendingTodos: "pendingTodos"
+    })
+    // completedTodos(){
+    //   return this.$store.getters.completedTodos
+    // },
+    // pendingTodos(){
+    //   return this.$store.getters.pendingTodos
+    // }
   }
 }
 </script>
